2d23021d066d6e3bb45070471fc168a3a74829ba
Xavier Jongwook Seong <ypwa121@gmail.com>
2025-05-11 17:23:39 +0900

    create jwsdapne2 vpc

---
+++ terraform/vpc/jwsd_apnortheast2/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket         = "testuser-apnortheast2-tfstate"
+    key            = "provisioning/terraform/vpc/jwsd_apnortheast2/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
